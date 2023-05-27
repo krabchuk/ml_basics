c31ea48c59731d420f89d00e71995e881ec87178
krabchuk <krabchuk@gmail.com>
2023-05-27 18:47:45 +0300

    NEW: Gradient boosting on top of Tree from sklearn

--- linear_models/models.py
+++ linear_models/models.py
@@ -9,8 +9,10 @@ class LinearModel:
         self.weights = None
         self.train_losses = None
 
-    def __make_iteration(self, X, y, lr, min_loss=None):
+    def __make_iteration(self, X, y, lr, min_loss=None, debug_print=False):
         y_pred = np.dot(X, self.weights)
+        if debug_print:
+            print(self.weights)
         loss = np.sum((y - y_pred) ** 2) / len(y)
         self.train_losses.append(loss)
         grad = np.dot(y_pred - y, X)
@@ -18,11 +20,11 @@ class LinearModel:
             return
         self.weights -= lr * grad
 
-    def fit(self, X, y, lr=0.01, iterations=1000, min_loss=1e-9):
+    def fit(self, X, y, lr=0.01, iterations=1000, min_loss=1e-9, debug_print=False):
         self.weights = np.random.rand(X.shape[1])
         self.train_losses = []
         for i in range(iterations):
-            self.__make_iteration(X, y, lr, min_loss)
+            self.__make_iteration(X, y, lr, min_loss, debug_print)
 
     def predict(self, X):
         return np.dot(X, self.weights)
@@ -37,7 +39,7 @@ class LinearModelWithAdditionalElement():
         y_pred = np.dot(X, self.weights)
         loss = np.sum((y - y_pred) ** 2) / len(y)
         self.train_losses.append(loss)
-        grad = np.dot(y_pred - y, X)
+        grad = 2 * np.dot(y_pred - y, X)
         if min_loss is not None and np.sum(np.abs(grad)) < min_loss:
             return
         self.weights -= lr * grad
